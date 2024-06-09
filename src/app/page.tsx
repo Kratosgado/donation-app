ca59abb90afa37f95687c1b3c2c6f9c127126600
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2024-06-09 12:04:30 +0000

    add (main): typewriter effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,11 @@ import { BackgroundBeams } from "@/components/ui/background-beans";
 import { InfiniteMovingCards } from "@/components/ui/infinite-moving-cards";
 import { LampDemo } from "@/components/lamp.effect";
 import Image from "next/image";
+import { TypewriterEffectSmooth } from "@/components/ui/typewriter.effect";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <Navbar className="top-2" />
       <InfiniteMovingCards
         items={testimonials}
@@ -16,12 +17,18 @@ export default function Home() {
         speed="slow"
       />
       <LampDemo />
-
+      {/* <div className="flex items-center justify-center"> */}
       <div className="h-[40rem] w-full flex items-center justify-center flex-row">
+        <TypewriterEffectSmooth
+          words={[
+            { text: "Donation" },
+            { text: "Options", className: "text-blue-500 dark:text-blue-500" },
+          ]}
+        />
         <PinContainer title="Food Donation" href="/donate/food">
           <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem]">
             <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-              Have food for spare?s
+              Have food for spare?
             </h3>
             <div className="text-base !m-0 !p-0 font-normal">
               <span className="text-slate-500 ">
@@ -45,6 +52,7 @@ export default function Home() {
           </div>
         </PinContainer>
       </div>
+      {/* </div> */}
       <BackgroundBeams />
     </main>
   );
